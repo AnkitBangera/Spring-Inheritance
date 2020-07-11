65fadfb869bf72c2e27cf8d9d5a6421fed7f30dc
ankit <ankit@DESKTOP-A4EU1SK>
2020-07-12 00:08:30 +0530

    Spring inheritance

---
+++ Spring Inheritance/src/org/Spring/Triangle.java
@@ -0,0 +1,43 @@
+package org.Spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	private ApplicationContext context=null;
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [pointA=" + pointA + ", pointB=" + pointB + ", pointC=" + pointC + "]";
+	}
+
+}
